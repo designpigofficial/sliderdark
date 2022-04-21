411270826179d61b2abcd5cd9034be60a53fb961
designpigofficial <89279319+designpigofficial@users.noreply.github.com>
2022-04-21 09:38:31 -0600

    final for starter

--- mail-it.php
+++ mail-it.php
@@ -3,7 +3,7 @@
 /* Code by David McKeown - craftedbydavid.com */
 /* Editable entries are bellow */
 
-$send_to = "mail@server.com";
+$send_to = "support@soprissolar.com";
 $send_subject = "Ajax form ";
 
 
